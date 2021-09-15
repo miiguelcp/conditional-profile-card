3f018edbcc2975333e1d49afa64237a7303d8e2b
Miiguelcp <miiguelcp09@gmail.com>
2021-09-15 17:17:33 -0400

    Primer commit

--- src/js/index.js
+++ src/js/index.js
@@ -33,14 +33,33 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>
+          ${variables.name ? variables.name : "Miguel"} 
+          ${variables.lastname ? variables.lastname : "Carreño"}
+          </h1>
+          <h2>${variables.role ? variables.role : "Web Developer"}</h2>
+          <h3>${variables.city ? variables.city : "Puerto Cabello"}, ${
+    variables.country ? variables.country : "Venezuela"
+  }</h3>
+          <ul class=${
+            variables.socialMediaPosition !== null
+              ? variables.socialMediaPosition
+              : ""
+          }>
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : ""
+            }" target="_blank"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : ""
+            }" target="_blank"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/${
+              variables.linkedin
+                ? variables.linkedin
+                : "miguel-carreño-000791219 "
+            }" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://www.instagram.com/${
+              variables.instagram ? variables.instagram : ""
+            }" target="_blank"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -56,7 +75,7 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/men/42.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
